606d23e88892633639c924c11a37a2f751116c0a
Cameron Drysdale <cameron@driz.co.uk>
2021-06-06 14:06:55 +0100

    updates

--- public/wp-content/themes/olano/page-content-development.php
+++ public/wp-content/themes/olano/page-content-development.php
@@ -3,44 +3,6 @@
 <section class="section section--green">
     <div class="container">
 
-        <div class="wp-block-columns are-vertically-aligned-center">
-            <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%;">
-                <img src="<?php echo bloginfo('template_directory'); ?>/assets/img/services/Green.svg">
-            </div>
-            <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:75%;">
-                <h1>Content development</h1>
-                <p>The goal is to create the perfect training for your business. Here's why elearning might be the right tool to achieve this.</p>
-            </div>
-        </div>
-
-        <div class="video">
-            <iframe width="1920" height="1080" src="https://www.youtube.com/embed/Koj3N4cupNE" title="YouTube video player"
-                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-
-    </div>
-</section>
-
-<section id="services" class="section section--off-white">
-    <div class="container">
-
-        <div class="wp-block-columns">
-            <div class="wp-block-column" style="flex-basis:25%;">
-                <h3 class="">Why elearning?</h3>
-            </div>
-            <div class="wp-block-column" style="flex-basis:75%;">
-                <p>Bespoke or Off-The-Shelf? Both can be great depending on your needs. The key is identifying your needs and tailoring the training to suit them.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam iusto reprehenderit perferendis amet quasi accusamus aliquid harum nihil sed aliquam inventore eligendi cum eveniet laboriosam porro error, tenetur vel a.</p>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione aut earum suscipit vero perferendis, accusamus libero blanditiis fugit vel magnam, natus sint temporibus sed! Corrupti tempore modi inventore velit provident.</p>
-            </div>
-        </div>
-
-    </div>
-</section>
-
-<section class="section section--yellow">
-    <div class="container">
-
     </div>
 </section>
 
